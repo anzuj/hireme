097303a222b9c0cc6b3c95922a9a132300aedf8f
Anzelika <36598337+anzuj@users.noreply.github.com>
2020-06-23 13:31:15 +0200

    Update globals.js

--- src/components/Reasons/globals.js
+++ src/components/Reasons/globals.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
 
-const components = require.context('.', true, /\.vue$/i)
+// Globally register all base components for convenience, because they
+// will be used very frequently. Components are registered using the
+// PascalCased version of their file name.
 
- components.keys().map(key => {
-     Vue.component(files(key).default.name ?? key.split('/').pop().split('.')[0], components(key).default);
- })
+// https://webpack.js.org/guides/dependency-management/#require-context
+const requireComponent = require.context(
+  // Look for files in the current directory
+  '.',
+  // Do not look in subdirectories
+  false,
+  // Only include "Reason-" prefixed .vue files
+  /Reason-[\w-]+\.vue$/
+)
 
+// For each matching file name...
+requireComponent.keys().forEach((fileName) => {
+  // Get the component config
+  const componentConfig = requireComponent(fileName)
+  // Get the component name
+  const componentName = fileName
+    // Remove the "./" from the beginning - did I do this right?
+    .replace(/^\.\/, '')
+    // Remove the file extension from the end
+    .replace(/\.\w+$/, '')
+
+
+  // Globally register the component
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
