9c5731bb655cbd02265dec4d4a2fa1ebb4370419
anzuj <anzelika91@gmail.com>
2020-06-25 00:17:09 +0200

    fix btn jitter

--- src/components/Reasons/Reason-10.vue
+++ src/components/Reasons/Reason-10.vue
@@ -6,28 +6,30 @@
         <v-card class="demoCard pt-2">
           <v-row justify="center">
             <v-btn :ripple="false" @click="showBad=!showBad">{{showBad?"hide":"show"}}</v-btn>
-            <v-img
-              v-show="showBad"
+            <div  v-show="showBad">
+            <v-img 
               class="mt-2"
               height="120"
               contain
               src="https://media.wired.com/photos/5cdefc28b2569892c06b2ae4/master/w_2560%2Cc_limit/Culture-Grumpy-Cat-487386121-2.jpg"
             ></v-img>
+            </div>
           </v-row>
         </v-card>
       </v-col>
       <v-col cols="12" sm="6">
         <v-card class="demoCard pt-2">
-          <v-row justify="center">
+          <v-row justify="center">      
             <v-btn :ripple="false" @click="showGood=!showGood">{{showGood?"hide":"show"}}</v-btn>
             <v-fade-transition>
+              <div v-show="showGood">
             <v-img
-              v-show="showGood"
               class="mt-2"
               height="120"
               contain
               src="https://i.pinimg.com/474x/bf/f5/d0/bff5d074d399bdfec6071e9168398406.jpg"
             ></v-img>
+            </div>
             </v-fade-transition>
           </v-row>
         </v-card>
@@ -47,11 +49,6 @@ export default {
     showGood: false
   }),
   methods: {},
-  computed: {
-    nr() {
-      return this.$store.state.reasonsAmount;
-    }
-  }
 };
 </script>
 
